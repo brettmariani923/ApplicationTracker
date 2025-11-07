54229c45f51c9aee5ba20907c388e0780aec2b4a
brettmariani923@gmail.com <brettmariani923@gmail.com>
2025-11-06 22:01:30 -0600

    planning/setting up tables

---
+++ ApplicationTracker.SqlServerDatabase/DBO/Tables/ApplicationEvents.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ApplicationEvents]
+(
+  EventId       BIGINT IDENTITY(1,1) PRIMARY KEY,
+  ApplicationId INT NOT NULL FOREIGN KEY REFERENCES Applications(ApplicationId) ON DELETE CASCADE,
+  -- "ON DELETE CASCADE" is so that if a record in the parent table is deleted, then all the related records in the child table will also be deleted
+  StageId       INT NOT NULL  FOREIGN KEY REFERENCES Stages(StageId),
+  OccurredUtc   DATETIME2 NOT NULL DEFAULT SYSUTCDATETIME(),
+);
---
+++ ApplicationTracker.SqlServerDatabase/DBO/Tables/Applications.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Applications]
+(
+  ApplicationId INT IDENTITY(1,1) PRIMARY KEY,
+  CompanyName   NVARCHAR(200) NOT NULL,
+  JobTitle      NVARCHAR(200) NULL,
+  ResumeId      NVARCHAR(50)  NULL,
+  CreatedUtc    DATETIME2 NOT NULL DEFAULT SYSUTCDATETIME(),
+);
