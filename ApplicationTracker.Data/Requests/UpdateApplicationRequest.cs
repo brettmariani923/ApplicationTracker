049b171e7c8b79180fe921d72428835ff5194ade
brettmariani923@gmail.com <brettmariani923@gmail.com>
2025-11-12 01:18:58 -0600

    added requests

---
+++ ApplicationTracker.Data/Requests/UpdateApplicationRequest.cs
@@ -0,0 +1,30 @@
+﻿using ApplicationTracker.Data.Interfaces;
+using ApplicationTracker.Data.Rows;
+
+namespace ApplicationTracker.Data.Requests
+{
+    public class UpdateApplicationRequest
+    {
+        private readonly IDataAccess _data;
+        private readonly Application_Row _row;
+        public UpdateApplicationRequest(IDataAccess data, Application_Row row)
+        {
+            _data = data;
+            _row = row;
+        }
+
+        public string GetSql() =>
+        
+            @"UPDATE dbo.Applications
+              SET CompanyName = @CompanyName,
+                     JobTitle = @JobTitle,
+              WHERE CompanyName = @CompanyName;";
+       
+
+        public object? GetParameters() => new
+        {
+            _row.CompanyName,
+            _row.JobTitle
+        };
+    }
+}
